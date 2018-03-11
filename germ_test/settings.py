fc51199db72d1166f3d107caf9cb3552dd61e6d3
Vitaly Svyatyuk <goodgame1945@gmail.com>
2018-03-11 21:45:29 +0300

    disable logging

--- germ_test/settings.py
+++ germ_test/settings.py
@@ -138,24 +138,6 @@ REST_FRAMEWORK = {
     ),
 }
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': '/var/log/django/debug.log',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
 
 try:
     from .local_settings import *
